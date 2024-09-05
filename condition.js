60be9aa03cb8d9565e15ee5dd1417a142f799de3
reyad-hasan <akmkamrulhasanreyad@gmail.com>
2024-09-06 01:32:04 +0600

    condition

---
+++ condition.js
@@ -0,0 +1,19 @@
+// var money = 250;
+// var biriyani = 300;
+
+// if(money >= biriyani){
+//     console.log('Ami biriyani khabo');
+// } else {
+//     console.log('Ami muri khabo');
+// };
+
+// let x = 11; 
+// if (x < 10) { x = x + 3; } 
+// else { x = x - 2; } 
+// console.log(x);
+
+
+// let result = 10 < "5";
+// console.log(result)
+
+console.log(6 != 6);
